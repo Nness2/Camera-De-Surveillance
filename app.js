cac862eaa136d1a069d1089a1bda91012e9fef4f
Aness Menaa <menaa.aness2@gmail.com>
2019-11-25 19:21:23 +0100

    blob stream

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+'use strict';
 var http = require('http');
 var fs = require('fs');
 
@@ -11,7 +12,7 @@ var server = http.createServer(function(req, res) {
 
 // Chargement de socket.io
 var io = require('socket.io').listen(server);
-
+//var reader  = new FileReader();
 // Quand un client se connecte, on le note dans la console
 io.sockets.on('connection', function (socket) {
     console.log('Un client est connecté !');
@@ -26,6 +27,15 @@ io.sockets.on('connection', function (socket) {
 	
 	socket.on('test', function(test) {
         socket.emit('mouvement', '');
+    });
+	
+	socket.on('stream', function(stream) {
+		//reader.readAsDataURL(stream);
+		//console.log(reader.result);
+        //var fileBuffer = new Buffer(stream, 'base64');
+		//fs.writeFileSync("fileName", fileBuffer);
+		//console.log(fileBuffer);
+		//fs.writeFileSync(fileName, fileBuffer);
     });
 });
 
